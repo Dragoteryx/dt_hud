3a1c0d5fa5d77ddb64278fe219806c781b54f912
Dragoteryx <dragoteryx@live.fr>
2022-06-19 23:03:57 +0200

    killfeed animations

--- lua/dt_hud/killfeed.lua
+++ lua/dt_hud/killfeed.lua
@@ -40,7 +40,10 @@ else
 
   local KILLFEED = {}
   net.Receive("DT_HUD/EntityDeath", function()
-    table.insert(KILLFEED, {
+    local duration = DT_HUD.KillfeedDuration:GetFloat()
+    local entry = {
+      time = CurTime(),
+      duration = duration,
       attacker = {
         Name = net.ReadString(),
         IsPlayer = net.ReadBool(),
@@ -51,9 +54,10 @@ else
         IsPlayer = net.ReadBool(),
         Disposition = net.ReadUInt(3)
       }
-    })
-    timer.Simple(DT_HUD.KillfeedDuration:GetFloat(), function()
-      table.remove(KILLFEED, 1)
+    }
+    table.insert(KILLFEED, entry)
+    timer.Simple(duration, function()
+      table.RemoveByValue(KILLFEED, entry)
     end)
   end)
 
@@ -72,16 +76,20 @@ else
     else ctx:SetOrigin(-23.5, 1) end
 
     for i = 1, math.min(#KILLFEED, DT_HUD.KillfeedMaximum:GetInt()) do
-      local death = KILLFEED[i]
+      local entry = KILLFEED[i]
+      local enterOffset = 24 - math.min(1, (CurTime() - entry.time) * 5) * 24
+      local leaveOffset = 24 - math.min(1, (entry.duration - (CurTime() - entry.time)) * 5) * 24
+      local offset = math.max(enterOffset, leaveOffset)
+      ctx:AddOffset(offset, 0)
       ctx:DrawFrame(22, 3, "right")
       ctx:CreateSquare(11, 1.5, 2):Fill(DT_HUD.MainColor.Value, DT_HUD.DeathIcon)
-      local attackerColor = DT_HUD.GetDispositionColor(death.attacker.Disposition)
-      local attackerText = death.attacker.IsPlayer and death.attacker.Name or language.GetPhrase(death.attacker.Name)
+      local attackerColor = DT_HUD.GetDispositionColor(entry.attacker.Disposition)
+      local attackerText = entry.attacker.IsPlayer and entry.attacker.Name or language.GetPhrase(entry.attacker.Name)
       ctx:DrawText(1, 0.85, attackerText, {color = attackerColor, maxLength = 8.5})
-      local victimColor = DT_HUD.GetDispositionColor(death.victim.Disposition)
-      local victimText = death.victim.IsPlayer and death.victim.Name or language.GetPhrase(death.victim.Name)
+      local victimColor = DT_HUD.GetDispositionColor(entry.victim.Disposition)
+      local victimText = entry.victim.IsPlayer and entry.victim.Name or language.GetPhrase(entry.victim.Name)
       ctx:DrawText(21, 0.85, victimText, {color = victimColor, maxLength = 8.5, xAlign = TEXT_ALIGN_RIGHT})
-      ctx:AddOffset(0, 4)
+      ctx:AddOffset(-offset, 4)
     end
   end)
 
